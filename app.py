e8f8bb639b2e18ac75087040d0ab83494db9916c
rahul25-25 <rahulyadavgangavennolla@gmail.com>
2025-05-23 13:55:00 -0700

    Updated greeting to 'Hello, Rahul'

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"  # You will later change this message
+    return "Hello, Rahul"
 
 if __name__ == '__main__':
     print("Starting Flask server...")
